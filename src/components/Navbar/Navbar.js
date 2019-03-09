e990bb06bda223f69f7966aa31fa18b62a868c70
Szymon Feliks <sz.feliks@gmail.com>
2019-03-09 14:43:09 +0100

    background color changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,13 +8,13 @@ export default function Navbar() {
             <img src={logo} alt="city-tourse logo"/>
             <ul className="nav-links">
                 <li>
-                    <a href="" className="nav-link">home</a>
+                    <a href="/" className="nav-link">home</a>
                 </li>
                 <li>
-                    <a href="" className="nav-link">about</a>
+                    <a href="/" className="nav-link">about</a>
                 </li>
                 <li>
-                    <a href="" className="nav-link active">tours</a>
+                    <a href="/" className="nav-link active">tours</a>
                 </li>
             </ul>
         </nav>
